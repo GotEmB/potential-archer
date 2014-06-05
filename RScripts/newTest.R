9e6020e31526bcb058e80265b788477bc904b8b3
Praphull Kumar <praphull27@gmail.com>
2014-06-04 20:53:02 -0700

    Updated gitignore file to ignore .DS_Store

--- RScripts/newTest.R
+++ RScripts/newTest.R
@@ -28,17 +28,39 @@ print(length(unique_languages))
 mongo_rmongodb <- mongo.create(host = "localhost")
 user_commit_activity <- data.frame(stringsAsFactors = FALSE)
 
-for (i in 1:length(unique_languages)) {
-  lang = unique_languages[i]
-  user_commit_act <- mongo.find(mongo_rmongodb, ns = "cs249.usercommitactvities", query=list(language=lang),fields=list(language=1L,repository=1L,author=1L,changes=1L))
-  while (mongo.cursor.next(user_commit_act))
-  {
-    item <- mongo.cursor.value(user_commit_act)
-    repository <- mongo.oid.to.string(mongo.bson.value(item, "repository"))
-    author <- mongo.oid.to.string(mongo.bson.value(item, "author"))
-    language <- mongo.bson.value(item, "language")
-    changes <- mongo.bson.value(item, "changes")
+user_commit_act <- mongo.find(mongo_rmongodb, ns="cs249.usercommitactvities", fields=list(language=1L,repository=1L,author=1L,changes=1L))
+while (mongo.cursor.next(user_commit_act))
+{
+  item <- mongo.cursor.value(user_commit_act)
+  repository <- mongo.oid.to.string(mongo.bson.value(item, "repository"))
+  author <- mongo.oid.to.string(mongo.bson.value(item, "author"))
+  language <- mongo.bson.value(item, "language")
+  changes <- mongo.bson.value(item, "changes")
+  if (class(repository) != "NULL" && class(author) != "NULL" && class(language) != "NULL" && class(changes) != "NULL") {
     user_commit_activity <- rbind(user_commit_activity, data.frame(author=author,repository=repository,language=language,changes=changes))
   }
 }
+
+ddply_projects_per_lang = ddply( user_commit_activity,
+                             c("language"), 
+                             summarize,        
+                             repo_count = count(repository)
+)
+print(head(ddply_projects_per_lang))
+
+ddply_changes_per_user = ddply( user_commit_activity,
+                                 c("language", "author"), 
+                                 summarize,        
+                                 total_changes = sum(changes),
+)
+print(head(ddply_changes_per_user))
+
+ddply_changes_per_user_lang = ddply( ddply_changes_per_user,
+                                     c("language"), 
+                                     summarize,        
+                                     avg_commits = mean(total_changes),
+)
+print(head(ddply_changes_per_user_lang))
+
+
 
